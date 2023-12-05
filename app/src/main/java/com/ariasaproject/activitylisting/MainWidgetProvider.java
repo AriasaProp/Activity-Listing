1f8d2bbe356dc72b36cdbaaf1bd7f84b05180901
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2023-12-05 22:06:14 +0800

    App Icon update

--- app/src/main/java/com/ariasaproject/activitylisting/MainWidgetProvider.java
+++ app/src/main/java/com/ariasaproject/activitylisting/MainWidgetProvider.java
@@ -5,17 +5,23 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainWidgetProvider extends AppWidgetProvider {
+    final SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        Date currentDate = new Date();
+        String td = timeFormat.format(currentDate);
         for (int appWidgetId : appWidgetIds) {
-        	/*
-			      RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-			      
-			      appWidgetManager.updateAppWidget(appWidgetId, views);
-			      */
+        	RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+	        views.setTextViewText(R.id.widget_state, td);
+	        appWidgetManager.updateAppWidget(appWidgetId, views);
         }
+
     }
 }
+
+
